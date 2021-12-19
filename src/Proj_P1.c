8419f33af00a547410b69291af51a3f79eba98a9
AHMAD SHBAT <ahmad.shbat93@e-uvt.ro>
2021-12-19 23:52:54 +0200

    finishing the split func with several forms

--- src/Proj_P1.c
+++ src/Proj_P1.c
@@ -44,12 +44,12 @@ char * readLineFromUser()
 
 char** splitArgumentsFromLine(char * readLineFromUser)
 {
-    if(NULL == *readLineFromUser)
+    if(NULL == readLineFromUser)
     {
         printf("invalide arg");
         return NULL;
     }
-    char * iter = *(&readLineFromUser);
+    char * iter = &(*readLineFromUser);
 
     char ** res;
     int sizeBuff1 = 16;
@@ -57,42 +57,131 @@ char** splitArgumentsFromLine(char * readLineFromUser)
     int index = 0;
     int index1 = 0;
 
-    *res = (char *) malloc (sizeof(char) * sizeBuff1);
+    res = (char **) malloc (sizeof(char*) * sizeBuff1);
 
     if(NULL == *res)
     {
         printf("cant allocate mem.");
         return NULL;
     }
-    while(*iter != '\0' || *iter != '\n')
+    do
     {
-        char c = *iter;
+        int c = *iter;
         char * oneArg = (char*) malloc (sizeof(char) * sizeBuff);
-        if(*iter == ' ')
+        if(c == ' ')
         {
             if(index1 >= sizeBuff1)
             {
-                *res = realloc(*res, sizeBuff1 * 2);
+                res = realloc(res, sizeBuff1 * 2);
                 sizeBuff1 *= 2;
             }
             res[index1] = oneArg;
             index1++;
             free(oneArg);
+            iter++;
 
         }
 
-        if(index >= sizeBuff + 1)
+        if(index >= sizeBuff && c != ' ')
         {
             oneArg = realloc(oneArg, sizeBuff * 2);
             sizeBuff *= 2;
         }
-       
-        oneArg[index] = c; 
-        index++;
-    }
+       if(c != ' ')
+       {
+            oneArg[index] = c; 
+            index++;
+            iter++;
+       }
+    }while(*iter != '\0' || *iter != '\n');
+    return res;
 
 
 }
+
+
+
+char** splitArgumentsFromLine(char * readLineFromUser)
+{
+    const char * dil;
+    char dilm = ' ';
+    dil = &dilm;
+    if(NULL == readLineFromUser)
+    {
+        printf("invalide arg");
+        return NULL;
+    }
+
+    char ** res;
+    char * oneArg;
+    int sizeBuff = 16;
+    int index = 0;
+
+    res = (char **) malloc (sizeof(char*) * sizeBuff);
+
+    if(NULL == *res)
+    {
+        printf("cant allocate mem.");
+        return NULL;
+    }
+    oneArg = strtok(readLineFromUser, dil);
+    while(NULL != oneArg)
+    {
+      if(index >= sizeBuff)
+      {
+          *res = realloc(*res, sizeBuff * 2);
+          if(NULL == *res)
+          {
+              printf("cant allocate mem.");
+              return NULL;
+          }
+          sizeBuff *= 2;
+          
+      }  
+      res[index] = oneArg;
+      index++;
+      oneArg = strtok(NULL, dil);
+    }
+    res[index] = NULL;
+    return res;
+
+
+}
+
+
+
+// #define LSH_TOK_BUFSIZE 64
+// #define LSH_TOK_DELIM " \t\r\n\a"
+// char** splitArgumentsFromLine(char * readLineFromUser)
+// {
+//   int bufsize = LSH_TOK_BUFSIZE, position = 0;
+//   char **tokens = malloc(bufsize * sizeof(char*));
+//   char *token;
+
+//   if (!tokens) {
+//     fprintf(stderr, "lsh: allocation error\n");
+//     exit(EXIT_FAILURE);
+//   }
+
+//   token = strtok(readLineFromUser, LSH_TOK_DELIM);
+//   while (token != NULL) {
+//     tokens[position] = token;
+//     position++;
+
+//     if (position >= bufsize) {
+//       bufsize += LSH_TOK_BUFSIZE;
+//       tokens = realloc(tokens, bufsize * sizeof(char*));
+//       if (!tokens) {
+//         fprintf(stderr, "lsh: allocation error\n");
+//         exit(EXIT_FAILURE);
+//       }
+//     }
+
+//     token = strtok(NULL, LSH_TOK_DELIM);
+//   }
+//   tokens[position] = NULL;
+//   return tokens;
+// }
 
 
 
@@ -115,7 +204,7 @@ void terminal_loop(bool* termStatus)
             printf("[ERROR] can not read line from the user");
             return;
         }
-        arguments = splitArgumentsFromLine(&*readLine);
+        arguments = splitArgumentsFromLine(*readLine);
         if(0 == *arguments)
         {
             printf("[ERROR] can not splits line into arguments");
@@ -144,9 +233,12 @@ int main(int argc, char ** argv)
     char * h = readLineFromUser();
     printf("%s", h);
     char ** g = splitArgumentsFromLine(h);
-    while(NULL != g)
+    char ** tmp = &(*g);
+    while(NULL != *tmp)
     {
-        printf("%s\n", *g);
+        printf("%s\n", *tmp);
+        tmp++;
     }
+    
     return 0;
 }
